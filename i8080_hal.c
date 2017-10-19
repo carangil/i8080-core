c6578bd200088b7f975fb6dbf27b422fd60de316
carangil <m@mwsherman.com>
2017-10-18 20:39:34 -0700

    move files around

--- i8080_hal.c
+++ i8080_hal.c
@@ -1,1 +1,101 @@
-modtest/i8080_hal.c
+// Intel 8080 (KR580VM80A) microprocessor core model
+//
+// Copyright (C) 2012 Alexander Demin <alexander@demin.ws>
+//
+// Credits
+//
+// Viacheslav Slavinsky, Vector-06C FPGA Replica
+// http://code.google.com/p/vector06cc/
+//
+// Dmitry Tselikov, Bashrikia-2M and Radio-86RK on Altera DE1
+// http://bashkiria-2m.narod.ru/fpga.html
+//
+// Ian Bartholomew, 8080/8085 CPU Exerciser
+// http://www.idb.me.uk/sunhillow/8080.html
+//
+// Frank Cringle, The origianal exerciser for the Z80.
+//
+// Thanks to zx.pk.ru and nedopc.org/forum communities.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include "i8080_hal.h"
+
+//unsigned char memory[MEMSIZE];
+
+unsigned int i8080_hal_memory_read_word(unsigned int addr) {
+    return 
+        (i8080_hal_memory_read_byte(addr + 1) << 8) |
+        i8080_hal_memory_read_byte(addr);
+}
+
+void i8080_hal_memory_write_word(unsigned int addr, unsigned int word) {
+    i8080_hal_memory_write_byte(addr, word & 0xff);
+    i8080_hal_memory_write_byte(addr + 1, (word >> 8) & 0xff);
+}
+ int i8080_pc();
+
+unsigned int i8080_hal_memory_read_byte(unsigned int addr) {
+	//printf(" R%x:%x\n", addr,memory[ ((unsigned int)addr) & MEMMASK] );
+    if (addr >= MEMSIZE) {
+                printf("ILLEGAL READ FROM %x PC=%x\n", addr, i8080_pc());
+                while(1);
+                return 0;
+    }
+    
+    return memory[ ((unsigned int)addr) & MEMMASK];
+}
+
+void i8080_hal_memory_write_byte(unsigned addr, unsigned int byte) {
+//	printf(" W%x=%x\n", addr, byte);
+        
+        if (addr >= MEMSIZE)
+                printf("ILLEGAL WRITE TO %x\n", addr);
+        
+    memory[((unsigned int)addr) &MEMMASK ] = byte;
+}
+
+int i8080_hal_io_input(int port) {
+   if (port==1)
+       return inkey();
+   
+        
+    
+   // printf(" read port 1 %d\n", port);       
+            
+  
+        
+    return 0;
+    
+    
+    
+}
+
+void i8080_hal_io_output(int port, int value) {
+      //  printf(" port %d byte %x  %c\n", port, value, value&0x7f);
+        
+        printf("%c", value&0x7f);
+        
+        
+    // Nothing.
+}
+
+void i8080_hal_iff(int on) {
+    // Northing.
+}
+
+unsigned char* i8080_hal_memory(void) {
+    return &memory[0];
+}
